d1cf7dc371b8822c187ccd0c4d7d5f99bff3b933
xSurfuel2 <xSurfuel2@gmail.com>
2025-09-29 10:38:35 +0200

    v0.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,25 @@
 package org.example;
 
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int[] array = new int[50];
+        Random rand = new Random();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Generar números aleatorios y guardarlos en el array
+        for (int i = 0; i < array.length; i++)
+        {
+            array[i] = rand.nextInt(100);
+            System.out.println(array[i]);
+
         }
+
+
+
     }
 }
